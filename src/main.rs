f0774a0194ac6a4a4cb1d8244d1336614b2fe455
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2022-06-13 23:56:34 +0100

    Add log settings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+///
+/// main.rs
+///
+use std::env;
+use env_logger::{Builder, Target};
+
+#[derive(Debug)]
+struct Config {
+    addr: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    Builder::new()
+        .target(Target::Stdout)
+        .init();
+
+    let args: Vec<String> = env::args().collect();
+    let config = parse_config(args);
+
+    println!("{:?}, addr: {}", config, config.addr);
+}
+
+fn parse_config(args: Vec<String>) -> Config {
+    if args.len() == 1 {
+        log::error!("No server address provided!");
+        return Config {addr: "No server".to_string()}
+    }
+
+    let addr = &args[1];
+    Config {addr: addr.to_string()}
 }
