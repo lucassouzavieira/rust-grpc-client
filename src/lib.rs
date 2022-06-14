192e9c1102ebe9012280e050d17a722622704f27
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2022-06-14 10:34:22 +0100

    Use derive to define CLI app structure

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,34 @@ pub mod fleet {
 pub mod incident {
     tonic::include_proto!("incident");
 }
+
+pub mod cli {
+    use clap::{Parser, Subcommand};
+
+    #[derive(Parser, Debug)]
+    #[clap(author = "Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>", version, about)]
+    /// A gRPC CLI client
+    pub struct Arguments {
+        #[clap(default_value = "localhost:9200", short, long)]
+        /// gRPC server address
+        addr: String,
+
+        #[clap(subcommand)]
+        cmd: SubCommand
+    }
+
+    #[derive(Subcommand, Debug)]
+    pub enum SubCommand {
+        /// Handles LFB fleet info
+        Fleet {
+            #[clap(short, long, default_value = "", )]
+            all: String
+        },
+
+        /// Handles LFB incidents info
+        Incident {
+            #[clap(short, long, default_value = "", )]
+            all: String
+        },
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,51 +3,14 @@
 ///
 ///
 use env_logger::{Builder, Target};
-use clap::{Arg, Command};
-use log::{error};
+use rust_grpc_client::cli;
+use clap::Parser;
 
 fn main() {
     Builder::new()
         .target(Target::Stdout)
         .init();
 
-    let cmd = Command::new("fleet")
-        .about("Returns info about LFB vehicles")
-        .subcommand_required(true)
-        .author("Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>")
-        .subcommand(
-            Command::new("list")
-                .short_flag('L')
-                .long_flag("list")
-                .about("List vehicles")
-                .arg(
-                    Arg::new("all")
-                        .short('a')
-                        .long("all")
-                        .help("List all vehicles")
-                        .takes_value(false)
-                )
-        ).arg(
-            Arg::new("server").short('s')
-                .long("server")
-                .help("gRPC server address")
-                .takes_value(true)
-                .default_value("localhost:9200")
-        ).arg_required_else_help(true);
-
-    let matches = cmd.get_matches();
-    // let args = cmd.get_arguments();
-
-    match matches.subcommand() {
-        Some(("list", list_matches)) => {
-            if list_matches.contains_id("all") {
-                // TODO implement
-                error!("List all vehicles action");
-                // for arg in args {
-                //     println!("{}", arg)
-                // }
-            }
-        }
-        _ => unreachable!(), // all subcommands failed
-    }
+    let args = cli::Arguments::parse();
+    println!("{:?}", args)
 }
