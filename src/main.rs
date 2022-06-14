de9c560898d7d3d4481e492c29754e92cdc3b4b7
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2022-06-14 09:40:31 +0100

    Structuring CLI app

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,53 @@
 ///
 /// main.rs
 ///
-use std::env;
+///
 use env_logger::{Builder, Target};
-
-#[derive(Debug)]
-struct Config {
-    addr: String
-}
+use clap::{Arg, Command};
+use log::{error};
 
 fn main() {
     Builder::new()
         .target(Target::Stdout)
         .init();
 
-    let args: Vec<String> = env::args().collect();
-    let config = parse_config(args);
+    let cmd = Command::new("fleet")
+        .about("Returns info about LFB vehicles")
+        .subcommand_required(true)
+        .author("Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>")
+        .subcommand(
+            Command::new("list")
+                .short_flag('L')
+                .long_flag("list")
+                .about("List vehicles")
+                .arg(
+                    Arg::new("all")
+                        .short('a')
+                        .long("all")
+                        .help("List all vehicles")
+                        .takes_value(false)
+                )
+        ).arg(
+            Arg::new("server").short('s')
+                .long("server")
+                .help("gRPC server address")
+                .takes_value(true)
+                .default_value("localhost:9200")
+        ).arg_required_else_help(true);
 
-    println!("{:?}, addr: {}", config, config.addr);
-}
+    let matches = cmd.get_matches();
+    // let args = cmd.get_arguments();
 
-fn parse_config(args: Vec<String>) -> Config {
-    if args.len() == 1 {
-        log::error!("No server address provided!");
-        return Config {addr: "No server".to_string()}
+    match matches.subcommand() {
+        Some(("list", list_matches)) => {
+            if list_matches.contains_id("all") {
+                // TODO implement
+                error!("List all vehicles action");
+                // for arg in args {
+                //     println!("{}", arg)
+                // }
+            }
+        }
+        _ => unreachable!(), // all subcommands failed
     }
-
-    let addr = &args[1];
-    Config {addr: addr.to_string()}
 }
